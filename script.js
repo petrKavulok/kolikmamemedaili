bae8a819abde98255bd559f9c93c1173cb2dcf6f
Petr Kavulok <petr.kavulok@ackee.cz>
2024-08-03 09:34:24 +0200

    ♻️ fetching from official api

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 
 async function fetchMedalData() {
-    // const url = 'https://olympics.com/OG2024/data/CIS_MedalNOCs~lang=ENG~comp=OG2024.json';
-    const url = 'https://api.olympics.kevle.xyz/medals?country=cze'
-    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-
+    const url = 'https://olympics.com/OG2024/data/CIS_MedalNOCs~lang=ENG~comp=OG2024.json';
+    
     try {
-        const response = await fetch(proxyUrl + url);
+        const response = await fetch(url);
         if (!response.ok) {
             throw new Error('Network response was not ok ' + response.statusText);
         }
         const data = await response.json();
-        displayMedalData(data);
+        const czeData = data['medalNOC'].filter(entry => entry.org === 'CZE' && entry.gender === 'TOT' && entry.sport === 'GLO');
+        console.log({czeData})
+
+        displayMedalData(czeData[0]);
 
     } catch (error) {
         console.error('There has been a problem with your fetch operation:', error);
@@ -19,14 +20,17 @@ async function fetchMedalData() {
 
 function displayMedalData(data) {
     const zlato = document.getElementById('zlato');
-    zlato.innerHTML = `<h2>🥇 ${data.results[0].medals.gold}</h2>`;
+    zlato.innerHTML = `<h2>🥇 ${data.gold}</h2>`;
     const stribro = document.getElementById('stribro');
-    stribro.innerHTML = `<h2>🥈 ${data.results[0].medals.silver}</h2>`;
+    stribro.innerHTML = `<h2>🥈 ${data.silver}</h2>`;
     const bronz = document.getElementById('bronz');
-    bronz.innerHTML = `<h2>🥉 ${data.results[0].medals.bronze}</h2>`;
+    bronz.innerHTML = `<h2>🥉 ${data.bronze}</h2>`;
     const total = document.getElementById('celkem');
-    console.log(data.results[0].medals.total)
-    total.innerText = data.results[0].medals.total;
+    total.innerText = data.total;
+
+    const titleElement = document.querySelector('head > title');
+    titleElement.innerText = `${titleElement.innerText} (${data.total})`
+
 }
 
 fetchMedalData();
