13f2ce7bc5b895bae433b3eb48ae3884dbaafc9b
Petr Kavulok <petr.kavulok@ackee.cz>
2024-08-05 10:07:05 +0200

    💩 wip types

--- src/utils/reduceMedals.tsx
+++ src/utils/reduceMedals.tsx
@@ -1,6 +1,8 @@
 import { fetchData } from "./fetchData";
 
+// @ts-expect-error
 export function reduceMedals(data): {gold: string[], silver: string[], bronze: string[]} {
+  // @ts-expect-error
     const medalsObject = data.reduce((acc, curr) => {
         if (curr.gold > 0) {
           acc.gold.push(curr.sport);
@@ -17,20 +19,24 @@ export function reduceMedals(data): {gold: string[], silver: string[], bronze: s
     return medalsObject
 }
 
+// @ts-expect-error
 export async function matchDisciplines(data) {
-    const response = await fetchData('https://olympics.com/OG2024/data/GLO_Disciplines~comp=OG2024~lang=ENG.json');
-   
-   // Create a mapping of sport codes to their descriptions
-    const sportCodeToDescription = response.disciplines.reduce((acc, discipline) => {
-        if (discipline.isSport) {
-        acc[discipline.code] = discipline.description;
-        }
-        return acc;
-    }, {});
-    
-    // Transform the medals object
-    const transformMedalsObject = (medalsObj, codeToDesc) => {
-        return Object.keys(medalsObj).reduce((acc, medalType) => {
+  const response = await fetchData('https://olympics.com/OG2024/data/GLO_Disciplines~comp=OG2024~lang=ENG.json');
+  
+  // Create a mapping of sport codes to their descriptions
+  // @ts-expect-error
+  const sportCodeToDescription = response.disciplines.reduce((acc, discipline) => {
+    if (discipline.isSport) {
+      acc[discipline.code] = discipline.description;
+    }
+    return acc;
+  }, {});
+  
+  // Transform the medals object
+  // @ts-expect-error
+  const transformMedalsObject = (medalsObj, codeToDesc) => {
+    return Object.keys(medalsObj).reduce((acc, medalType) => {
+          // @ts-expect-error
         acc[medalType] = medalsObj[medalType].map(code => codeToDesc[code] || code);
         return acc;
         }, {});
