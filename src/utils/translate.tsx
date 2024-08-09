e6da99f11a7c2edf486560a4c1469567f54f726a
Petr Kavulok <petr.kavulok@ackee.cz>
2024-08-09 14:42:26 +0200

    🌐

--- src/utils/translate.tsx
+++ src/utils/translate.tsx
@@ -1,14 +1,15 @@
 const sporty = {
     Fencing: 'Šerm',
     Tennis: 'Tenis',
-    'Canoe Slalom': 'Kayak slalom'
+    'Canoe Slalom': 'Kajak slalom',
+    'Canoe Sprint': 'Rychlostní kanoistika'
 } as const
 
 export const translate = (sport: any): string[] => {
     // @ts-expect-error
     const arr = []
     sport.map((el: keyof typeof sporty) => {
-        console.log(el, sporty[el])
+        // console.log(el, sporty[el])
         if(sporty[el]) {
             arr.push(sporty[el])
         } else {
